6143b22e709e01f0897d7d69ff12a2378d3378ba
Hannes <h@nnes.dev>
2021-11-02 19:51:11 +0000

    Fix order of imports

--- src/game/enemy.rs
+++ src/game/enemy.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 use bevy_ecs_tilemap::prelude::*;
 use rand::seq::SliceRandom;
 
-use crate::game::components::{Drawable, CanPlayerMove, Position};
+use crate::game::components::{CanPlayerMove, Drawable, Position};
 use crate::game::config::*;
 use crate::game::tilemap::*;
 
